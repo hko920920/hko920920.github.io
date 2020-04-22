8a5c9b3d30e6bfb9e7057775b56df8be25b57e60
hko920920 <55654922+hko920920@users.noreply.github.com>
2020-04-22 19:54:41 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,236 @@
 $(document).ready(function () {
-    $('.menu-toggler').on('click', function () {
-        $(this).toggleClass('open');
-        $('.top-nav').toggleClass('open');
-    });
+    
+/*Active spy side bar=====================================*/
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("main section");
 
-    $('.top-nav .nav-link').on('click', function () {
-        $('.menu-toggler').removeClass('open');
-        $('.top-nav').removeClass('open');
-    });
+let lastId;
+let cur = [];
 
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+//    _.throttle(doThatStuff, 100);
+// });
+// Only not doing it here to keep this Pen dependency-free.
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop +400 &&
+      section.offsetTop + section.offsetHeight > fromTop +400
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
+    
+    
+//Fashion SHow Modal///////////////////////////////
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+    else if(event.target == modal1) {
+    modal1.style.display = "none";
+  }
+    else if(event.target == modal2) {
+    modal2.style.display = "none";
+  }
+    else if(event.target == modal3) {
+    modal3.style.display = "none";
+  }
+    
+}
+//Fashion SHow Modal///////////////////////////////
+
+
+
+//Stritch Modal///////////////////////////////
+
+// Get the modal
+var modal1 = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+//Stritch Modal///////////////////////////////
+
+
+
+//Camscon Modal///////////////////////////////
+
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+//Camscon Modal///////////////////////////////
+
+//EZEN Modal///////////////////////////////
+
+// Get the modal
+var modal3 = document.getElementById("myModal3");
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close3")[0];
+
+// When the user clicks the button, open the modal 
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+//EZEN Modal///////////////////////////////
+
+
+    
+/*AOS =======================================*/
     AOS.init({
         easing:'ease',
         duration:1800,
         once: true
     });
-
-    $('#up').on('click', function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 2000);
-    });
-
+    
+    
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+    autoplay: true,
+  autoplaySpeed: 2000,
 });
 
+$('.slider-for1').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav1'
+});
+$('.slider-nav1').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for1',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+    autoplay: true,
+  autoplaySpeed: 2000,
+});    
+    
+$('.slider-for2').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav2'
+});
+$('.slider-nav2').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for2',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+    autoplay: true,
+  autoplaySpeed: 2000,
+});        
+
+$('.slider-for3').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav3'
+});
+$('.slider-nav3').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for3',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+    autoplay: true,
+  autoplaySpeed: 2000,
+});        
+    
+
+
+$( "#tabs" ).tabs();
+  
+      
+    
+});
 (function(){
     var current =0;
     var max =0;
@@ -66,6 +274,7 @@ $(document).ready(function () {
 
     jQuery (document).ready(init);
 })();
+
 (function(){
     var current =0;
     var max =0;
@@ -99,137 +308,6 @@ $(document).ready(function () {
     }
     function animate(){
         var moveX=current *799;
-        TweenMax. to(container, 0.8, {marginLeft:-moveX, ease:Expo.easeOut} );
-
-        clearInterval(interval);
-        interval = setInterval(next, 6000);
-
-    }
-
-
-    jQuery (document).ready(init);
-})();
-
-(function(){
-    var current =0;
-    var max =0;
-    var container;
-    var interval;
-    function init(){
-        container =jQuery(".Marketing ul");
-        max = container.children().length;
-
-        console.log("init");
-        interval = setInterval(next, 4000);
-
-        events();
-
-    }
-    function events(){
-        jQuery("button.prev3").on("click", prev);
-        jQuery("button.next3").on("click", next);
-    }
-
-    function prev(e){
-        current--;
-        if(current < 0) current = max-1;
-        animate();
-    }
-    function next(e){
-        current++;
-        if(current > max-1) current = 0;
-        animate();
-
-    }
-    function animate(){
-        var moveX=current *1225;
-        TweenMax. to(container, 0.8, {marginLeft:-moveX, ease:Expo.easeOut} );
-
-        clearInterval(interval);
-        interval = setInterval(next, 4000);
-
-    }
-
-
-    jQuery (document).ready(init);
-})();
-
-(function(){
-    var current =0;
-    var max =0;
-    var container;
-    var interval;
-    function init(){
-        container =jQuery(".Theater ul");
-        max = container.children().length;
-
-        console.log("init");
-        interval = setInterval(next, 4000);
-
-        events();
-
-    }
-    function events(){
-        jQuery("button.prev4").on("click", prev);
-        jQuery("button.next4").on("click", next);
-    }
-
-    function prev(e){
-        current--;
-        if(current < 0) current = max-1;
-        animate();
-    }
-    function next(e){
-        current++;
-        if(current > max-1) current = 0;
-        animate();
-
-    }
-    function animate(){
-        var moveX=current *900;
-        TweenMax. to(container, 0.8, {marginLeft:-moveX, ease:Expo.easeOut} );
-
-        clearInterval(interval);
-        interval = setInterval(next, 4000);
-
-    }
-
-
-    jQuery (document).ready(init);
-})();
-(function(){
-    var current =0;
-    var max =0;
-    var container;
-    var interval;
-    function init(){
-        container =jQuery(".cam ul");
-        max = container.children().length;
-
-        console.log("init");
-        interval = setInterval(next, 6000);
-
-        events();
-
-    }
-    function events(){
-        jQuery("button.prev5").on("click", prev);
-        jQuery("button.next5").on("click", next);
-    }
-
-    function prev(e){
-        current--;
-        if(current < 0) current = max-1;
-        animate();
-    }
-    function next(e){
-        current++;
-        if(current > max-1) current = 0;
-        animate();
-
-    }
-    function animate(){
-        var moveX=current *1000;
         TweenMax. to(container, 0.8, {marginLeft:-moveX, ease:Expo.easeOut} );
 
         clearInterval(interval);
